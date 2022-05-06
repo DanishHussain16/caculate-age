0446ca16545fba16c919cb0cddfdbeebf69b8cee
Danish Hussain <danishaljnab12345@gmail.com>
2022-05-06 22:45:51 +0500

    Add files via upload

---
+++ calculate age/app.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('forms').addEventListener('submit',function(e){
+    var date= document.getElementById('prompt').value;
+              var age=2022-parseInt(date);
+              
+
+e.preventDefault();
+});
